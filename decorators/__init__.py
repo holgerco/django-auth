e0d451105659da0054ad5899ff6f9fe721cef926
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2019-12-11 01:35:23 +0330

    Create timer decorator

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -2,6 +2,6 @@ from .anonymous import anonymous_required
 from .group import group_required
 from .superuser import superuser_only
 from .ajax import ajax_required
-
+from .timer import timeit
 
 # TODO : Convert to class based decorator
