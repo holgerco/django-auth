a4867dfb7294a3d27916f381816ad1fc1b202e64
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2019-12-11 01:34:43 +0330

    Create ajax decorator

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -1,6 +1,7 @@
 from .anonymous import anonymous_required
 from .group import group_required
 from .superuser import superuser_only
+from .ajax import ajax_required
 
 
 # TODO : Convert to class based decorator
