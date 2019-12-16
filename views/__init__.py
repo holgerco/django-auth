788bef6e3004a9fbb95e8ced17607603ee2001a2
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2019-12-16 22:16:28 +0330

    Add signup view

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,4 @@
 from .status_handlers import page_not_found, permission_denied, server_error, unauthorized, bad_request, maintenance
 from .login import login
 from .logout import logout
+from .signup import signup
