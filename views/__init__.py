f369579c987f53e0228aed959393feb00f3cff57
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2019-12-16 22:32:22 +0330

    Add verify email view

--- views/__init__.py
+++ views/__init__.py
@@ -2,3 +2,4 @@ from .status_handlers import page_not_found, permission_denied, server_error, un
 from .login import login
 from .logout import logout
 from .signup import signup
+from .verfiy_email_user import verify_email
