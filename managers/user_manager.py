a9e8a4398fb2dfa93a37ea870890d2314aa71c3c
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2019-12-19 09:29:14 +0330

    Create jwt decoder method for get user from jwt token

--- managers/user_manager.py
+++ managers/user_manager.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils import timezone
 from datetime import datetime
+import jwt
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -35,3 +37,26 @@ class UserManager(BaseUserManager):
             raise ValueError('Superuser must have is_superuser=True.')
 
         return self.create_user(email, password, **extra_fields)
+
+    def get_by_token(self, token: str, algorithm='HS256'):
+        encoded_token = token.encode('utf-8')
+        try:
+            decoded: dict = jwt.decode(encoded_token, settings.SECRET_KEY, algorithm)
+            user_id = decoded.get('id', None)
+            if user_id:
+                user = self.get(pk=user_id)
+                return user
+            user_email = decoded.get('email', None)
+            if user_email:
+                user = self.get(email=user_email)
+                return user
+            user_username = decoded.get('username', None)
+            if user_username:
+                user = self.get(username=user_username)
+                return user
+        except jwt.ExpiredSignatureError:
+            return None  # Signature expired. Please log in again.
+        except jwt.InvalidTokenError:
+            return None  # Invalid token. Please log in again.
+        except self.model.DoesNotExist:
+            return None  # User does not exist
