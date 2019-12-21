5f843d37cc0436e9c6f985b28b8d6d7ff2d17d7a
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2019-12-21 17:00:19 +0330

    Create jwt authentication required decorator

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -3,5 +3,6 @@ from .group import group_required
 from .superuser import superuser_only
 from .ajax import ajax_required
 from .timer import timeit
+from .jwt import jwt as jwt_required
 
 # TODO : Convert to class based decorator
