0644ced095d62dba10d27a58199911eb62364105
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2019-12-23 03:24:51 +0330

    Fix date verify bug

--- managers/user_manager.py
+++ managers/user_manager.py
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_verify', True)
-        extra_fields.setdefault('date_register', datetime.now())
+        extra_fields.setdefault('date_verify', datetime.now())
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
