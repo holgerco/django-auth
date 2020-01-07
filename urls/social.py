1114d2793b03348a0b958d07abb1a58c6f4c8261
mohammad hosein shamsaei <mh.shamsaei.ms@gmail.com>
2020-01-07 13:28:38 +0330

    Fix social auth bug

--- urls/social.py
+++ urls/social.py
@@ -1,19 +1,12 @@
 from django.conf.urls import url
 from authlib import views
-from authlib.facebook import FacebookOAuth2Client
-from authlib.google import GoogleOAuth2Client
-from authlib.twitter import TwitterOAuthClient
+from django.conf import settings
 
-urlpatterns = [
-    url(
-        r"^oauth/facebook/$",
-        views.oauth2,
-        {
-            "client_class": FacebookOAuth2Client,
-        },
-        name="accounts_oauth_facebook",
-    ),
-    url(
+urlpatterns = []
+
+if getattr(settings, 'GOOGLE_CLIENT_ID', '') and getattr(settings, 'GOOGLE_CLIENT_SECRET', ''):
+    from authlib.google import GoogleOAuth2Client
+    urlpatterns += url(
         r"^oauth/google/$",
         views.oauth2,
         {
@@ -21,7 +14,21 @@ urlpatterns = [
         },
         name="accounts_oauth_google",
     ),
-    url(
+
+if getattr(settings, 'FACEBOOK_CLIENT_ID', '') and getattr(settings, 'FACEBOOK_CLIENT_SECRET', ''):
+    from authlib.facebook import FacebookOAuth2Client
+    urlpatterns += url(
+        r"^oauth/facebook/$",
+        views.oauth2,
+        {
+            "client_class": FacebookOAuth2Client,
+        },
+        name="accounts_oauth_facebook",
+    ),
+
+if getattr(settings, 'TWITTER_CLIENT_ID', '') and getattr(settings, 'TWITTER_CLIENT_SECRET', ''):
+    from authlib.twitter import TwitterOAuthClient
+    urlpatterns += url(
         r"^oauth/twitter/$",
         views.oauth2,
         {
@@ -29,4 +36,3 @@ urlpatterns = [
         },
         name="accounts_oauth_twitter",
     )
-]
